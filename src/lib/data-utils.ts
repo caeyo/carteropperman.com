e5ae7cb50e6fba3d6e3ec7d4795eba98fae23b0d
caeyo <oppermancarter@gmail.com>
2025-07-16 22:07:52 -0500

    Rip out template content + add skeleton for my content

--- src/lib/data-utils.ts
+++ src/lib/data-utils.ts
@@ -1,10 +1,6 @@
 import { getCollection, render, type CollectionEntry } from 'astro:content'
 import { readingTime, calculateWordCountFromHtml } from '@/lib/utils'
 
-export async function getAllAuthors(): Promise<CollectionEntry<'authors'>[]> {
-  return await getCollection('authors')
-}
-
 export async function getAllPosts(): Promise<CollectionEntry<'blog'>[]> {
   const posts = await getCollection('blog')
   return posts
@@ -189,23 +185,6 @@ export async function getParentPost(
   const parentId = getParentId(subpostId)
   const allPosts = await getAllPosts()
   return allPosts.find((post) => post.id === parentId) || null
-}
-
-export async function parseAuthors(authorIds: string[] = []) {
-  if (!authorIds.length) return []
-
-  const allAuthors = await getAllAuthors()
-  const authorMap = new Map(allAuthors.map((author) => [author.id, author]))
-
-  return authorIds.map((id) => {
-    const author = authorMap.get(id)
-    return {
-      id,
-      name: author?.data?.name || id,
-      avatar: author?.data?.avatar || '/static/logo.png',
-      isRegistered: !!author,
-    }
-  })
 }
 
 export async function getPostById(
